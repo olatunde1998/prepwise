8cd411a2bd49c28ca7e833af3bd6188f3dfb2be4
geodevcode <geodevinstitute@gmail.com>
2025-06-14 10:14:27 +0100

    feat: wrap AuthForm in a styled container for sign-in and sign-up pages

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,11 @@
 import AuthForm from "@/components/AuthForm";
 
 const Page = () => {
-  return <AuthForm type="sign-in" />;
+  return (
+    <div className="flex items-center justify-center mx-auto max-w-7xl min-h-screen max-sm:px-4 max-sm:py-8">
+      <AuthForm type="sign-in" />
+    </div>
+  );
 };
 
 export default Page;
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,11 @@
 import AuthForm from "@/components/AuthForm";
 
 const Page = () => {
-  return <AuthForm type="sign-up" />;
+  return (
+    <div className="flex items-center justify-center mx-auto max-w-7xl min-h-screen max-sm:px-4 max-sm:py-8">
+      <AuthForm type="sign-up" />
+    </div>
+  );
 };
 
 export default Page;
