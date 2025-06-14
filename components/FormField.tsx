ae65e3fdf5a7e52d9e96075c0f159c0bad1feb15
geodevcode <geodevinstitute@gmail.com>
2025-06-14 09:13:22 +0100

    feat: add DisplayTechIcons and FormField components for improved UI functionality

---
+++ components/FormField.tsx
@@ -0,0 +1,50 @@
+import { Controller, Control, FieldValues, Path } from "react-hook-form";
+
+import {
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+interface FormFieldProps<T extends FieldValues> {
+  control: Control<T>;
+  name: Path<T>;
+  label: string;
+  placeholder?: string;
+  type?: "text" | "email" | "password";
+}
+
+const FormField = <T extends FieldValues>({
+  control,
+  name,
+  label,
+  placeholder,
+  type = "text",
+}: FormFieldProps<T>) => {
+  return (
+    <Controller
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel className="!text-light-100 !font-normal">
+            {label}
+          </FormLabel>
+          <FormControl>
+            <Input
+              className="!bg-dark-200 !rounded-full !min-h-12 !px-5 placeholder:!text-light-100"
+              type={type}
+              placeholder={placeholder}
+              {...field}
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
+
+export default FormField;
