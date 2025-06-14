e28b5ae79d79376e57e3180660d2fcb41aa0beda
geodevcode <geodevinstitute@gmail.com>
2025-06-14 10:15:18 +0100

    feat: add Layout and Home components for user authentication and interview management

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import Image from "next/image";
+import { ReactNode } from "react";
+import { redirect } from "next/navigation";
+import "../globals.css";
+import { auth } from "@/auth";
+
+
+const Layout = async ({ children }: { children: ReactNode }) => {
+  const session = await auth();
+  const userId = session?.user?.id as string;
+  const token = session?.accessToken as string;
+
+  if (!token || !userId) {
+    redirect("/");
+  }
+
+  return (
+    <div className="flex mx-auto max-w-7xl flex-col gap-12 my-12 px-16 max-sm:px-4 max-sm:my-8">
+      <nav>
+        <Link href="/" className="flex items-center gap-2">
+          <Image src="/logo.svg" alt="MockMate Logo" width={38} height={32} />
+          <h2 className="text-primary-100">PrepWise</h2>
+        </Link>
+      </nav>
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
---
+++ app/(root)/page.tsx
@@ -0,0 +1,105 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import InterviewCard from "@/components/InterviewCard";
+import { Button } from "@/components/ui/button";
+import { getUserById } from "../actions";
+import { auth } from "@/auth";
+
+import {
+  getInterviewsByUserId,
+  getLatestInterviews,
+} from "@/lib/actions/general.action";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const session = await auth();
+  const token = session?.accessToken as string;
+  const userId = session?.user?.id as string;
+
+  if (!token || !userId) {
+    redirect("/sign-in");
+  }
+
+  const user = await getUserById(userId);
+
+  const [userInterviews, allInterview] = await Promise.all([
+    getInterviewsByUserId(user?.id!),
+    getLatestInterviews({ userId: user?.id! }),
+  ]);
+
+  const hasPastInterviews = userInterviews?.length! > 0;
+  const hasUpcomingInterviews = allInterview?.length! > 0;
+
+  return (
+    <>
+      <section className="flex flex-row bg-gradient-to-b from-[#171532] to-[#08090D] rounded-3xl px-16 py-6 items-center justify-between max-sm:px-4">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
+          <p className="text-lg">
+            Practice real interview questions & get instant feedback
+          </p>
+
+          <Button
+            asChild
+            className="w-fit !bg-primary-200 !text-dark-100 hover:!bg-primary-200/80 !rounded-full !font-bold px-5 cursor-pointer min-h-10 max-sm:w-full"
+          >
+            <Link href="/interview">Start an Interview</Link>
+          </Button>
+        </div>
+
+        <Image
+          src="/robot.png"
+          alt="robo-dude"
+          width={400}
+          height={400}
+          className="max-sm:hidden"
+        />
+      </section>
+
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Your Interviews</h2>
+
+        <div className="flex flex-wrap gap-4 max-lg:flex-col w-full items-stretch">
+          {hasPastInterviews ? (
+            userInterviews?.map((interview) => (
+              <InterviewCard
+                key={interview.id}
+                userId={user?.id}
+                interviewId={interview.id}
+                role={interview.role}
+                type={interview.type}
+                techstack={interview.techstack}
+                createdAt={interview.createdAt}
+              />
+            ))
+          ) : (
+            <p>You haven&apos;t taken any interviews yet</p>
+          )}
+        </div>
+      </section>
+
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Take Interviews</h2>
+
+        <div className="flex flex-wrap gap-4 max-lg:flex-col w-full items-stretch">
+          {hasUpcomingInterviews ? (
+            allInterview?.map((interview) => (
+              <InterviewCard
+                key={interview.id}
+                userId={user?.id}
+                interviewId={interview.id}
+                role={interview.role}
+                type={interview.type}
+                techstack={interview.techstack}
+                createdAt={interview.createdAt}
+              />
+            ))
+          ) : (
+            <p>There are no interviews available</p>
+          )}
+        </div>
+      </section>
+    </>
+  );
+}
