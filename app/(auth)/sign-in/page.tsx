7f0cda7b2abcb38067e536c4e59721413fac6c60
geodevcode <geodevinstitute@gmail.com>
2025-06-23 16:14:33 +0100

    refactor: replace AuthForm with SignIn component in sign-in page

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,9 +1,9 @@
-import AuthForm from "@/components/AuthForm";
+import SignIn from "@/components/SignIn";
 
 const Page = () => {
   return (
     <div className="flex items-center justify-center mx-auto max-w-7xl min-h-screen max-sm:px-4 max-sm:py-8">
-      <AuthForm type="sign-in" />
+      <SignIn />
     </div>
   );
 };
