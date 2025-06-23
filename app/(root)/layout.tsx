8cf8ed97b558bbb0d9a009fc8f6e516712ab7e96
geodevcode <geodevinstitute@gmail.com>
2025-06-23 15:10:03 +0100

    refactor: replace logo link with NavBar component in Layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,8 @@
 import { redirect } from "next/navigation";
+import NavBar from "@/components/Navbar";
 import { ReactNode } from "react";
-import Image from "next/image";
 import { auth } from "@/auth";
-import Link from "next/link";
 import "../globals.css";
-
 
 const Layout = async ({ children }: { children: ReactNode }) => {
   const session = await auth();
@@ -17,11 +15,8 @@ const Layout = async ({ children }: { children: ReactNode }) => {
 
   return (
     <div className="flex mx-auto max-w-7xl flex-col gap-12 my-12 px-16 max-sm:px-4 max-sm:my-8">
-      <nav>
-        <Link href="/" className="flex items-center gap-2">
-          <Image src="/logo.svg" alt="MockMate Logo" width={38} height={32} />
-          <h2 className="text-primary-100">PrepWise</h2>
-        </Link>
+      <nav className="mb-10">
+        <NavBar />
       </nav>
       {children}
     </div>
